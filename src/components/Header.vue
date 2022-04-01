2f6279b1f6dbe77d0d6a3bd1448a286dce50ffd7
rowl <airowl.gasga@gmail.com>
2022-04-01 16:43:10 +0200

    aggiorno header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,47 @@
 <template>
     <header>
-        <h1>HEADER</h1>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand">LOGO</a>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="userInput">
+                    <button class="btn btn-outline-success" type="submit" @click='apiMovies()'>Search</button>
+                </form>
+                <h1>{{userInput}}</h1>
+            </div>
+        </nav>
     </header>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'mainHeader',
+    data: function(){
+        return{
+            userInput: '',
+            moviesArray: null
+        }
+    },
+    created() {
+    },
+    methods: {
+        apiMovies() {
+
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=&', { params: {query: this.userInput}} )
+            .then((result) => {
+                this.moviesArray = result.data.results;
+                console.log(this.moviesArray);
+            })
+            .catch(
+                console.error('error')
+            );
+        }
+    },
+    computed: {
+        
+    }
 }
 </script>
 

eb258e256555dfcb13759966e941f482d5dde9ba
rowl <airowl.gasga@gmail.com>
2022-04-06 13:55:27 +0200

    milestone 4 stilizzato la main con le cover

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,15 +1,54 @@
 <template>
-    <div>
-
+    <div class="cover col-12 col-sm-6 col-md-3 col-xxl-2">
+        <img :src="'https://image.tmdb.org/t/p/' + 'w300/' + imgPoster" :alt="title">
+        <div class="text-cover">
+            <p><strong>Titolo:</strong> {{title}}</p>
+            <p><strong>Titolo Originale:</strong> {{originalTitle}}</p>
+            <p><strong>Voto:</strong> <font-awesome-icon icon="fa-solid fa-star" v-for="n in voteIngers(voteAverage)" :key="n"/></p>
+            <p><strong>Overview:</strong> {{overview}}</p>
+            
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'showCover'
+    name: 'showCover',
+    props: {
+        title: String,
+        originalTitle: String,
+        originalLanguage: String,
+        voteAverage: Number,
+        imgPoster: String,
+        overview: String
+    },
+    methods: {
+        voteIngers(vote){
+            const voteInt = vote / 2;
+            return parseInt(voteInt);
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    div.cover{
+        color: white;
+        height: 25rem;
+        margin-bottom: 2rem;
 
+        img{
+            width: 100%;
+            height: 100%;
+            display: none;
+        }
+
+        .text-cover{
+            width: 100%;
+            height: 100%;
+            background-color: black;
+            padding: 1rem;
+            overflow: scroll;
+        }
+    }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,20 @@
 <template>
     <main>
         <h1>...</h1>
-        <Cover />
-        <ul>
-            <li v-for="(element, index) in result" :key="index">
-                <p>{{element.title}}</p>
-                <p>{{element.original_title}}</p>
-                <p>
-                    {{element.original_language}}
-                    <country-flag :country="element.original_language" size="normal"/>
-                </p>
-                <font-awesome-icon icon="fa-solid fa-star" v-for="(element, index) in voteIngers(element.vote_average)" :key="index"/>
-                <img :src="'https://image.tmdb.org/t/p/' + 'w300/' + element.poster_path" :alt="element.title">
-                <p>{{element.poster_path}}</p>
-            </li>
-        </ul>
+        <div class="container-fluid">
+            <div class="row">
+            <Cover      
+                v-for="(element, index) in result" 
+                :key="index" 
+                :title="element.title"
+                :originalTitle="element.original_title"
+                :originalLanguage="element.original_language"
+                :voteAverage="element.vote_average"
+                :imgPoster="element.poster_path"
+                :overview="element.overview"
+            />
+            </div>
+        </div>
     </main>
 </template>
 
@@ -37,10 +37,7 @@ export default {
 
     },
     methods: {
-        voteIngers(vote){
-            const voteInt = vote / 2;
-            return parseInt(voteInt);
-        }
+        
     },
     computed: {
         
